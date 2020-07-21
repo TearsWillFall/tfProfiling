4e272a382a49763d2349b5b0df94a65d965d0c84
Osvaldas Vainauskas <osvaldas.v@um.es>
2020-07-21 13:43:30 +0200

    Random seed fix

--- R/analysis.R
+++ R/analysis.R
@@ -80,7 +80,7 @@ analyze_tfbs_around_position=function(bin_path="tools/bedtools2/bin/bedtools",bi
 
   if ((max_regions) !=0 & max_regions<nrow(ref_data)){
     print(paste("Total TFBS > Maximum Number of regions to analyze",paste0("(",max_regions,")")))
-    print(paste("Random sampling",max_regions,"regions from all TFBS"))
+    print(paste("Random sampling",max_regions,"regions from all TFBS with seed",char2seed(tf_name,set=FALSE)))
     char2seed(tf_name,set=TRUE)
     ref_data=ref_data[sample(c(1:nrow(ref_data)),max_regions),]
 
@@ -129,7 +129,6 @@ accessibility_score=function(file="",output_dir=""){
     cov_data$LOW<-get_low_signal(cov_data$MEAN_DEPTH)
     cov_data$HIGH<-get_high_signal(cov_data$MEAN_DEPTH,cov_data$LOW)
 
-    stats=()
     n<-cov_data$TFBS_ANALYZED[1]
     range<-max(cov_data$HIGH) - min(cov_data$LOW)
     peaks<-find_peaks(cov_data$HIGH,m=20)
