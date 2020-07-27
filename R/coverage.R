f8daec55debf2a4383f0a15f1f3f01be87e7d1e9
Osvaldas Vainauskas <osvaldas.v@um.es>
2020-07-27 13:59:57 +0200

    a

--- R/coverage.R
+++ R/coverage.R
@@ -170,7 +170,7 @@ calculate_coverage_tfbs=function(bin_path="tools/samtools/samtools",ref_data="",
 
   if(!(nrow(cov_data)==(start+end+1))){
     fix=(as.numeric(tfbs_data[5])-start):(as.numeric(tfbs_data[5])+end)
-    fix=data.frame(chr=tfbs_data[1],pos=unique(fix,cov_data$pos))
+    fix=data.frame(chr=tfbs_data[1],pos=unique(fix,as.numeric(cov_data$pos)))
     cov_data=dplyr::bind_rows(cov_data,fix) %>% dplyr::arrange(pos)
   }
   cov_data=cbind(cov_data,strand=tfbs_data[4])
