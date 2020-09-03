d1b105ecb7690b7f5391a71f369fddbbd591cc12
Osvaldas Vainauskas <osvaldas.v@um.es>
2020-09-03 15:16:54 +0200

    Fix

--- R/coverage.R
+++ R/coverage.R
@@ -171,6 +171,12 @@ calculate_coverage_tfbs=function(bin_path="tools/samtools/samtools",ref_data="",
 
 
 
+  if(!(nrow(cov_data)==(start+end+1))){
+    fix=(as.numeric(tfbs_data[5])-start):(as.numeric(tfbs_data[5])+end)
+
+    fix=data.frame(chr=tfbs_data[1],pos=fix[!fix==cov_data$pos])
+    cov_data=dplyr::bind_rows(cov_data,fix) %>% dplyr::arrange(pos)
+  }
   cov_data=cbind(cov_data,strand=tfbs_data[4])
   cov_data=cov_data %>% dplyr::mutate(cor_cov=as.numeric(cov)/as.numeric(mean_cov))  %>% dplyr::mutate(norm_cor_cov=ifelse(cor_cov<cov_limit,cor_cov/norm_cov,NA),pos_relative_to_tfbs=dplyr::if_else(strand=="+",pos-as.numeric(tfbs_data[5]),-(pos-as.numeric(tfbs_data[5])))) %>% dplyr::arrange(pos_relative_to_tfbs)
 
