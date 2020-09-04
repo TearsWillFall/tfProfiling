3ff55e73e3e697f45c6cc7f6c497869bd7056d96
Osvaldas Vainauskas <osvaldas.v@um.es>
2020-09-04 17:36:22 +0200

    f

--- R/coverage.R
+++ R/coverage.R
@@ -47,6 +47,7 @@ calculate_genowide_coverage=function(bin_path="tools/bedtools2/bin/bedtools",bam
 
 get_norm_local_coverage=function(pos="",chr="",norm_log2=""){
    norm=as.numeric(norm_log2[norm_log2$chr==chr & pos>norm_log2$start & pos<norm_log2$end,]$log2)
+   warning(paste0(norm,pos,chr,sep="/"))
    if(!is.na(norm)){
      norm=norm^2
      if (norm==0){
