31c08632e0ee67e904eba4dc47c3daa0ad84b4d6
Osvaldas Vainauskas <osvaldas.v@um.es>
2021-03-10 20:12:42 +0000

    Fix

--- R/methylation.R
+++ R/methylation.R
@@ -58,9 +58,9 @@ calculate_MR_tfbs=function(bin_path="tools/PileOMeth/output/MethylDackel",ref_da
 		strand="--keepStrand"
 	}
 	if (verbose){
-		print(paste(bin_path," extract ",ref_genome, bam,"-l ",sub(";","\\;",sub("&","\\&",paste0(output_dir,sep,sample_name,"_",tf_name),".bed.tmp"))," -o ",out_file,strand," -q ",mapq," -p ",phred," -@ ",threads))
+		print(paste(bin_path," extract ",ref_genome, bam,"-l ",sub(";","\\\\;",sub("&","\\\\&",paste0(output_dir,sep,sample_name,"_",tf_name),".bed.tmp"))," -o ",out_file,strand," -q ",mapq," -p ",phred," -@ ",threads))
 	}
-	system(paste(bin_path," extract ",ref_genome, bam,"-l ",sub(";","\\;",sub("&","\\&",paste0(output_dir,sep,sample_name,"_",tf_name),".bed.tmp"))," -o ",out_file,strand," -q ",mapq," -p ",phred," -@ ",threads))
+	system(paste(bin_path," extract ",ref_genome, bam,"-l ",sub(";","\\\\;",sub("&","\\\\&",paste0(output_dir,sep,sample_name,"_",tf_name),".bed.tmp"))," -o ",out_file,strand," -q ",mapq," -p ",phred," -@ ",threads))
 	system(paste0("rm ",paste0(output_dir,sep,sample_name,"_",tf_name,".bed.tmp")))
 	tfbs=read.table(paste0(out_file,"_CpG.bedGraph"),skip=1)
 	tfbs$pos=as.integer((tfbs$V2+tfbs$V3)/2)
