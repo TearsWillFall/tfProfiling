caf1ca3056601cc6fed1fed68651946af9dcadfb
Osvaldas Vainauskas <osvaldas.v@um.es>
2021-03-10 11:56:56 +0000

    Fix

--- R/methylation.R
+++ R/methylation.R
@@ -60,7 +60,7 @@ calculate_MR_tfbs=function(bin_path="tools/PileOMeth/output/MethylDackel",ref_da
 		print(paste(bin_path," extract ",ref_genome, bam,"-l ",paste0(output_dir,sep,sample_name,"_",tf_name,".bed.tmp")," -o ",out_file,strand," -q ",mapq," -p ",phred))
 	}
 	system(paste(bin_path," extract ",ref_genome, bam,"-l ",paste0(output_dir,sep,sample_name,"_",tf_name,".bed.tmp")," -o ",out_file,strand," -q ",mapq," -p ",phred))
-	system(paste0("rm",paste0(output_dir,sep,sample_name,"_",tf_name,".bed.tmp")))
+	system(paste0("rm ",paste0(output_dir,sep,sample_name,"_",tf_name,".bed.tmp")))
 	tfbs=read.table(paste0(out_file,"_CpG.bedGraph"),skip=1)
 	tfbs$pos=as.integer((tfbs$V2+tfbs$V3)/2)
 	colnames(tfbs)=c("chr","start","end","MR","nC","nT","pos")
