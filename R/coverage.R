78293a71a27e3fb4bbb55f44581d33c4eeb7568d
Osvaldas Vainauskas <osvaldas.v@um.es>
2021-09-16 19:02:37 +0100

    Fix

--- R/coverage.R
+++ R/coverage.R
@@ -273,7 +273,7 @@ calculate_coverage_around_gp=function(bin_path="tools/samtools/samtools",chr="",
       cov_data_left_flank=read.csv(text=system(paste(bin_path,"view -c -q",mapq,bam,paste0(chr,":",as.numeric(position)-start,"-",as.numeric(position)-start_bin)),intern=TRUE),header=FALSE,sep="\t",stringsAsFactors=FALSE)
       cov_data_left_flank=data.frame(cov=cov_data_left_flank,bin="LEFT_FLANK",bin_pos=paste0(chr,":",as.numeric(position)-start,"-",as.numeric(position)-start_bin))
       cov_data_right_flank=read.csv(text=system(paste(bin_path,"view -c -q",mapq,bam, paste0(chr,":",as.numeric(position)+end_bin,"-",as.numeric(position)+end)),intern=TRUE),header=FALSE,sep="\t",stringsAsFactors=FALSE)
-      cov_data_right_flank=data.frame(cov=cov_data__flank,bin="RIGHT_FLANK",bin_pos=paste0(chr,":",as.numeric(position)+end_bin,"-",as.numeric(position)+end))
+      cov_data_right_flank=data.frame(cov=cov_data_flank,bin="RIGHT_FLANK",bin_pos=paste0(chr,":",as.numeric(position)+end_bin,"-",as.numeric(position)+end))
       cov_data=dplyr::bind_rows(list(cov_data_central,cov_data_left_flank,cov_data_right_flank))
       cov_data=cov_data %>% dplyr::mutate(cor_cov=as.numeric(cov)/mean_cov)  %>% dplyr::mutate(norm_cor_cov=cor_cov/as.numeric(norm_log2),bin=dplyr::if_else(strand=="+"|strand=="",bin,dplyr::if_else(bin=="RIGHT_FLANK","LEFT_FLANK","RIGHT_FLANK"))) %>% dplyr::arrange(bin)
       return(cov_data)
